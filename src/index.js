62d51ded23a758556a707939b843e88c60771ca0
AliaksandrChachura <skif198560@gmail.com>
2019-03-16 15:31:32 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length <= 1)
+        return false;
+    let openingBrackets = [];
+    let closingBrackets = [];
+    for (let i = 0; i < bracketsConfig.length; i ++){
+        openingBrackets.push(bracketsConfig[i][0]);
+        closingBrackets.push(bracketsConfig[i][1]);
+    }
+    let matchingOpeningBracket, ch;
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        ch = str[i];
+        if (closingBrackets.indexOf(ch) > -1) {
+            matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)];
+            if (stack.length === 0 || (stack.pop() !== matchingOpeningBracket)) {
+                return false;
+            }
+        } else {
+            stack.push(ch);
+        }
+    }
+
+    return (stack.length === 0);
+
 }
