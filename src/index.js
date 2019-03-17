937ae23990b898f173e1110832710fb192af388f
AliaksandrChachura <skif198560@gmail.com>
2019-03-17 21:25:10 +0300

    At once without mistakes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ module.exports = function check(str, bracketsConfig) {
         return false;
     let openingBrackets = [];
     let closingBrackets = [];
+    let sameBrackets = [];
     for (let i = 0; i < bracketsConfig.length; i ++){
-        openingBrackets.push(bracketsConfig[i][0]);
-        closingBrackets.push(bracketsConfig[i][1]);
+        if (bracketsConfig[i][0] === bracketsConfig[i][1]){
+            sameBrackets.push(bracketsConfig[i][0]);
+        }
+        else {
+            openingBrackets.push(bracketsConfig[i][0]);
+            closingBrackets.push(bracketsConfig[i][1]);
+        }
     }
     let matchingOpeningBracket, ch;
     let stack = [];
@@ -17,11 +23,21 @@ module.exports = function check(str, bracketsConfig) {
             if (stack.length === 0 || (stack.pop() !== matchingOpeningBracket)) {
                 return false;
             }
-        } else {
+
+        }
+        else if (sameBrackets.indexOf(ch) > -1){
+            if (stack[stack.length-1] === ch) {
+                stack.pop();
+            }
+            else {
+                stack.push(ch);
+            }
+        }
+        else {
             stack.push(ch);
         }
     }
 
     return (stack.length === 0);
 
-}
+};
